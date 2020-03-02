d4ccdf38125c0b41f73182cf81a68fac549caeac
Aatish Bhatia <aatish@gmail.com>
2020-03-02 12:13:13 -0500

    automatic update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run daily to update covid stats
-jupyter nbconvert --to notebook --execute curvefit.ipynb
 mv curvefit.ipnyb curvefit.previous.ipnyb
+jupyter nbconvert --to notebook --execute curvefit.ipynb
 mv curvefit.nbconvert.ipynb curvefit.ipynb
 git add *
 git commit -m 'automatic update'
