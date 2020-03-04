36bb093c6405e020bd3de1a10b441db3d74ac33a
Aatish Bhatia <aatish@gmail.com>
2020-03-04 13:49:29 -0500

    automatic update

--- update.sh
+++ update.sh
@@ -6,11 +6,6 @@ jupyter nbconvert --to notebook --execute curvefit.ipynb
 mv curvefit.ipynb curvefit.previous.ipynb
 mv curvefit.nbconvert.ipynb curvefit.ipynb
 
-jupyter nbconvert --to notebook --execute curvefit-predictions.ipynb
-
-mv curvefit-predictions.ipynb curvefit-predictions.previous.ipynb
-mv curvefit-predictions.nbconvert.ipynb curvefit-predictions.ipynb
-
 git add *
 git commit -m 'automatic update'
 git push
